22922126de77e5212138704d8d2394d68af06439
chensssss <cherrychen1@shunshunliuxue.com>
2020-07-28 10:03:15 +0800

    set

---
+++ testNow/index.js
@@ -0,0 +1,11 @@
+const patch = require('patch')
+module.exports = class TestNow{
+/**
+ * 生成测试文件名
+ * @param {*} filename  代码文件名
+ */
+  getTestFileName(filename){
+    // 输出方法名
+    return ''
+  }
+}
